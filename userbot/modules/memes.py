c34f52d51c0f3ee20192671645f1aa1105163c28
aidilaryanto <57789597+aidilaryanto@users.noreply.github.com>
2020-02-18 21:45:45 +0800

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -1300,6 +1300,7 @@ async def nou(e):
                                                                                     
 CMD_HELP.update({
     "memes":
+
 \n\n.cowsay\
 \nUsage: cow which says things.\
 \n\n:/\
