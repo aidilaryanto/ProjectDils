da7aba56ba82d9ca43f48927d26243d46a4c621b
aidilaryanto <aidilaryanto18@gmail.com>
2020-03-29 10:38:42 +0800

    updater: rebranding

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -84,7 +84,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             await event.edit(f'{txt}\n`Here is the error log:\n{error}`')
             return repo.__del__()
         await event.edit('`Successfully Updated!\n'
-                         'Restarting, please wait...`')
+                         'DilBot is restarting, please wait...`')
     else:
         await event.edit('`[HEROKU]:'
                          '\nPlease set up` **HEROKU_API_KEY** `variable.`'
@@ -99,7 +99,7 @@ async def update(event, repo, ups_rem, ac_br):
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
     await event.edit('`Successfully Updated!\n'
-                     'Bot is restarting... Wait for a second!`')
+                     'DilBot is restarting... Wait for a second!`')
     # Spin a new instance of bot
     args = [sys.executable, "-m", "userbot"]
     execle(sys.executable, *args, environ)
@@ -158,7 +158,7 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n`Your USERBOT is`  **up-to-date**  `with`  **{UPSTREAM_REPO_BRANCH}**\n')
+            f'\n`DilBot is`  **up-to-date**  `with`  **{UPSTREAM_REPO_BRANCH}**\n')
         return repo.__del__()
 
     if conf is None and force_update is False:
@@ -182,7 +182,7 @@ async def upstream(event):
         await event.edit(
             '`Force-Syncing to latest stable userbot code, please wait...`')
     else:
-        await event.edit('`Updating userbot, please wait....`')
+        await event.edit('`Updating DilBot, please wait....`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
     elif conf == "deploy":
