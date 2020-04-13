f0b80c7f82fbf8a8637faae4c92cc8031094ecc5
aidilaryanto <aidilaryanto18@gmail.com>
2020-04-13 17:01:17 +0800

    DilBot: bump version to 2.3

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 
-LOGS.info("You are running DilBot [v2.2.2]")
+LOGS.info("You are running DilBot [v2.3]")
 
 LOGS.info(
     "DilBot is now running !!" 
