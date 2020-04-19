8149a6c4460575a21a063f79d920ad9abd5c2b51
aidilaryanto <aidilaryanto18@gmail.com>
2020-04-20 02:41:16 +0800

    Deepfry: update and fix

--- userbot/modules/deepfry.py
+++ userbot/modules/deepfry.py
@@ -1,45 +1,64 @@
-#Ported by heinzdf@fortifying
+#Ported by Adek Maulana
 
-import datetime
+import os
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot import bot, CMD_HELP
-from userbot.events import register
 
-@register(outgoing=True, pattern="^.df(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
+from userbot.events import register
+from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
+
+
+@register(outgoing=True, pattern='^.df(:? |$)')
+async def _(fry):
+    await fry.edit("`Sending information...`")
+    if fry.fwd_from:
+        return
+    if not fry.reply_to_msg_id:
+        await fry.edit("`Reply to any user message photo...`")
+        return
+    reply_message = await fry.get_reply_message()
     if not reply_message.media:
-       await event.edit("```Reply to text message```")
-       return
-    chat = "@image_deepfrybot"
-    sender = reply_message.sender
+        await fry.edit("`No image found to fry...`")
+        return
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
-    await event.edit("```Processing```")
-    async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
-              await bot.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @image_deepfrybot and try again```")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.delete()   
-             await bot.forward_messages(event.chat_id, response.message)
+        await fry.edit("`Reply to actual user...`")
+        return
+    chat = "@image_deepfrybot"
+    message_id_to_reply = fry.message.reply_to_msg_id
+    async with fry.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(events.NewMessage(incoming=True,
+                                       from_users=432858024))
+            msg = await fry.client.send_message(chat, reply_message)
+            response = await response
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await fry.reply("`Please unblock` @image_deepfrybot`...`")
+            return
+        if response.text.startswith("Forward"):
+            await fry.edit("`Please disable your forward privacy setting...`")
+        else:
+            downloaded_file_name = await fry.client.download_media(
+                                 response.message.media,
+                                 TEMP_DOWNLOAD_DIRECTORY
+            )
+            await fry.client.send_file(
+                fry.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply
+            )
+            """ - cleanup chat after completed - """
+            await fry.client.delete_messages(conv.chat_id,
+                                             [msg.id, response.id])
+            await fry.delete()
+            return os.remove(downloaded_file_name)
+
 
 CMD_HELP.update({
-        "df":
-        "`>.df`"
-        "\nUsage: Krispi image😂👌."
+    "deepfry":
+    ">`.df`"
+    "\nUsage: deepfry image/sticker from the reply."
+    "\n@image_deepfrybot"
 })
