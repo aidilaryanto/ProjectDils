021cddb8e3506276965ec7196d549fed69c91b07
aidilaryanto <aidilaryanto18@gmail.com>
2020-04-20 01:00:55 +0800

    ProjectDils: bump version to 2.4

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 
-LOGS.info("You are running ProjectDils [v2.3]")
+LOGS.info("You are running ProjectDils [v2.4]")
 
 LOGS.info(
     "ProjectDils is now running !!" 
