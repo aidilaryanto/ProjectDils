12e53f32301a293491321f4b7136349be0a02b01
aidilaryanto <aidilaryanto18@gmail.com>
2020-05-18 22:43:05 +0800

    scrapers: add var IMG_LIMIT to set image limit

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -160,6 +160,10 @@ TERM_ALIAS = os.environ.get("TERM_ALIAS", None)
 # Genius Lyrics API
 GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN", None)
 
+# IMG Stuff
+IMG_LIMIT = os.environ.get("IMG_LIMIT", None)
+
+
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
 if not os.path.exists('bin'):
