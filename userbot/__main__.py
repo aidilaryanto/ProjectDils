bfb6d6ca32dbf6fb549351d5991f4dc79b7babf1
aidilaryanto <aidilaryanto18@gmail.com>
2020-05-31 02:18:11 +0800

    ProjectDils: bump version to 2.6
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 
-LOGS.info("You are running ProjectDils [v2.5]")
+LOGS.info("You are running ProjectDils [v2.6]")
 
 LOGS.info(
     "ProjectDils is now running !!" 
