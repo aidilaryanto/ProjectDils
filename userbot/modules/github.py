0c8073aa783bd3c492e1dc111e3c09207bb1f670
aidilaryanto <aidilaryanto18@gmail.com>
2020-05-31 02:17:38 +0800

    github: merge gitupload module
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -1,7 +1,15 @@
+from github import Github
 import aiohttp
+import asyncio
+import os
+import time
+from datetime import datetime
+from telethon import events
+from telethon.tl.types import DocumentAttributeVideo
 from userbot.events import register
-from userbot import CMD_HELP
+from userbot import CMD_HELP, GITHUB_ACCESS_TOKEN, GIT_REPO_NAME, bot
 
+GIT_TEMP_DIR = "./projectdils/temp/"
 
 @register(pattern=r".git (.*)", outgoing=True)
 async def github(event):
@@ -43,9 +51,77 @@ async def github(event):
 
                 await event.edit(REPLY)
 
+@register(outgoing=True, pattern="^.commit(?: |$)(.*)")
+async def download(event):
+    if event.fwd_from:
+        return	
+    if GITHUB_ACCESS_TOKEN is None:
+        await event.edit("`Please ADD Proper Access Token from github.com`") 
+        return   
+    if GIT_REPO_NAME is None:
+        await event.edit("`Please ADD Proper Github Repo Name of your userbot`")
+        return 
+    mone = await event.reply("Processing ...")
+    if not os.path.isdir(GIT_TEMP_DIR):
+        os.makedirs(GIT_TEMP_DIR)
+    start = datetime.now()
+    reply_message = await event.get_reply_message()
+    try:
+        c_time = time.time()
+        print("Downloading to TEMP directory")
+        downloaded_file_name = await bot.download_media(
+                reply_message.media,
+                GIT_TEMP_DIR
+            )
+    except Exception as e: 
+        await mone.edit(str(e))
+    else:
+        end = datetime.now()
+        ms = (end - start).seconds
+        await event.delete()
+        await mone.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+        await mone.edit("Committing to Github....")
+        await git_commit(downloaded_file_name, mone)
+
+async def git_commit(file_name,mone):        
+    content_list = []
+    access_token = GITHUB_ACCESS_TOKEN
+    g = Github(access_token)
+    file = open(file_name,"r",encoding='utf-8')
+    commit_data = file.read()
+    repo = g.get_repo(GIT_REPO_NAME)
+    print(repo.name)
+    create_file = True
+    contents = repo.get_contents("")
+    for content_file in contents:
+        content_list.append(str(content_file))
+        print(content_file)
+    for i in content_list:
+        create_file = True
+        if i == 'ContentFile(path="'+file_name+'")':
+            return await mone.edit("`File Already Exists`")
+            create_file = False
+    file_name = "userbot/modules/" + file_name		
+    if create_file == True:
+        file_name = file_name.replace("./projectdils/temp/","")
+        print(file_name)
+        try:
+            repo.create_file(file_name, "ProjectDils: Add new module", commit_data, branch="master")
+            print("Committed File")
+            ccess = GIT_REPO_NAME
+            ccess = ccess.strip()
+            await mone.edit(f"`Commited On ProjectDils Repo`\n\n[Your Modules](https://github.com/{ccess}/tree/master/{file_name})")
+        except:    
+            print("Cannot Create Module")
+            await mone.edit("Cannot Upload Module")
+    else:
+        return await mone.edit("`Committed Suicide`")
+
 
 CMD_HELP.update({
-    "git":
+    "github":
     ">`.git <username>`"
     "\nUsage: Like .whois but for GitHub usernames."
+    "\n\n>`.commit <reply to module file>`"
+    "\nUsage: GITHUB File Uploader."
 })
