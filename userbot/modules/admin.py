3661ff0295b9e7c057c7516e96e3dbd8f1ba7090
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2020-07-22 21:17:17 +0530

    admin: mute: improve
    *handle crashes
    *import modules to catch exception
    
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -12,8 +12,10 @@ from os import remove
 from telethon.errors import (BadRequestError, ChatAdminRequiredError,
                              ImageProcessFailedError, PhotoCropSizeSmallError,
                              UserAdminInvalidError)
-from telethon.errors.rpcerrorlist import (UserIdInvalidError,
-                                          MessageTooLongError)
+from telethon.errors.rpcerrorlist import (BadRequestError,
+                                          MessageTooLongError,
+                                          UserIdInvalidError,
+                                          UserAdminInvalidError)
 from telethon.tl.functions.channels import (EditAdminRequest,
                                             EditBannedRequest,
                                             EditPhotoRequest)
@@ -380,7 +382,7 @@ async def unmoot(unmot):
                 f"CHAT: {unmot.chat.title}(`{unmot.chat_id}`)")
 
 
-@register(incoming=True)
+@register(incoming=True, disable_errors=True)
 async def muter(moot):
     """ Used for deleting the messages of muted people """
     try:
@@ -403,9 +405,12 @@ async def muter(moot):
     if muted:
         for i in muted:
             if str(i.sender) == str(moot.sender_id):
-                await moot.delete()
-                await moot.client(
-                    EditBannedRequest(moot.chat_id, moot.sender_id, rights))
+                try:
+                    await moot.delete()
+                    await moot.client(
+                        EditBannedRequest(moot.chat_id, moot.sender_id, rights))
+                except (BadRequestError, UserAdminInvalidError, ChatAdminRequiredError, UserIdInvalidError):
+                    await moot.client.send_read_acknowledge(moot.chat_id, moot.id)
     for i in gmuted:
         if i.sender == str(moot.sender_id):
             await moot.delete()
