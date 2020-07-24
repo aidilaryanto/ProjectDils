45c65699ec7138a924cdb6f68612bc7f20e56113
Angga <angga@linuxmail.org>
2020-07-24 19:42:53 +0800

    ProjectDils: add justwatch
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -184,6 +184,9 @@ GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN") or None
 # IMG Stuff
 IMG_LIMIT = os.environ.get("IMG_LIMIT") or None
 
+# JustWatch Country
+WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY") or None
+
 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
