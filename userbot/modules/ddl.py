ef15f47e9de7e9cf1b71e60890eed5af5ca5595a
aidilaryanto <aidilaryanto18@gmail.com>
2020-08-01 20:32:29 +0800

    ddl: cleanup message after complete
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/modules/ddl.py
+++ userbot/modules/ddl.py
@@ -25,7 +25,7 @@ async def ddl(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=994325826)
             )
-            await bot.forward_messages(chat, reply_message)
+            msg = await bot.forward_messages(chat, reply_message)
             response = await response
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -35,7 +35,9 @@ async def ddl(event):
     await event.client.send_message(
         event.chat_id, response.message, reply_to=event.message.reply_to_msg_id
     )
-
+    await event.client.delete_messages(
+        conv.chat_id, [msg.id, response.id]
+    )
 
 CMD_HELP.update({
     "ddl": ">`.ddl`"
