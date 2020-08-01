9db1113a1888dd3df4593eb55c27d4f47af7be1d
Angga <angga@linuxmail.org>
2020-08-01 22:17:38 +0700

    ddl: add exceptions
    Change-Id: Ib807af2a47de2c507ce2f4a3876b51a3c0a88ee4
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/modules/ddl.py
+++ userbot/modules/ddl.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2020 AnggaR96s.
 # All rights reserved.
+from asyncio.exceptions import TimeoutError
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
@@ -18,26 +20,30 @@ async def ddl(event):
     if not reply_message.media:
         await event.edit(r"`¯\_ (ツ) _/¯`")
         return
-    await event.edit("```Generating direct link..```")
-    async with bot.conversation("@jnckbot") as conv:
-        chat = "@jnckbot"
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=994325826)
-            )
-            msg = await bot.forward_messages(chat, reply_message)
-            response = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply("```Unblock @jnckbot plox```")
-            return
-        await event.delete()
-    await event.client.send_message(
-        event.chat_id, response.message, reply_to=event.message.reply_to_msg_id
-    )
-    await event.client.delete_messages(
-        conv.chat_id, [msg.id, response.id]
-    )
+    await event.edit("`Generating direct link..`")
+    try:
+        async with bot.conversation("@jnckbot") as conv:
+            chat = "@jnckbot"
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=994325826)
+                )
+                msg = await bot.forward_messages(chat, reply_message)
+                response = await response
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Unblock @jnckbot plox`")
+                return
+            await event.delete()
+        await event.client.send_message(
+            event.chat_id, response.message, reply_to=event.message.reply_to_msg_id
+        )
+        await event.client.delete_messages(
+            conv.chat_id, [msg.id, response.id]
+        )
+    except TimeoutError:
+        return await event.edit("`Error: `@jnckbot` is not responding!.`")
+
 
 CMD_HELP.update({
     "ddl": ">`.ddl`"
