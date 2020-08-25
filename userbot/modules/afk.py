4628b44611e809262d8aa9a77a7b0f594c1af277
Mr.Miss <keselekpermen69@gmail.com>
2020-08-25 17:32:55 +0800

    afk: disable linting on import line

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -10,9 +10,18 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
-                     BOTLOG_CHATID, USERS, PM_AUTO_BAN)
 from userbot.events import register
+
+from userbot import (  # noqa pylint: disable=unused-import isort:skip
+    AFKREASON,
+    BOTLOG,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    COUNT_MSG,
+    ISAFK,
+    PM_AUTO_BAN,
+    USERS,
+)
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
