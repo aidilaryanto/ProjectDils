13006eaca50b0b2c46b032dae68a6744beaaaeed
aidilaryanto <aidilaryanto18@gmail.com>
2020-09-03 11:23:48 +0800

    system_stats: change auto delete to 40seconds
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -256,14 +256,14 @@ async def amireallyalive(alive):
             logo = ALIVE_LOGO
             await alive.delete()
             msg = await bot.send_file(alive.chat_id, logo, caption=output)
-            await asyncio.sleep(25)
+            await asyncio.sleep(40)
             await msg.delete()
         except BaseException:
             await alive.edit(
                 output + "\n\n *`The provided logo is invalid."
                 "\nMake sure the link is directed to the logo picture`"
             )
-            await asyncio.sleep(25)
+            await asyncio.sleep(40)
             await alive.delete()
     else:
         await alive.edit(output)
